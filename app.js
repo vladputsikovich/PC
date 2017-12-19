86e9ff1805ce723e38bdfed8f8302711934dca65
влад пуцкович <vladputsikovich@gmail.com>
2017-12-20 00:10:45 +0300

    непонтный коммит

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var fortune = require('./lib/fortune.js');
 var express = require('express');
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
@@ -12,8 +13,7 @@ app.get('/' , function(req , res){
 });
 
 app.get('/about', function(req ,res){
-    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];    
-    res.render('about', {fortune: randomFortune});
+    res.render('about', {fortune: fortune.getFortune()});
 });
 
 // пользовательская страница 404 
@@ -35,10 +35,3 @@ app.listen(app.get('port'), function(){
     app.get('port') + '; нажмите Ctrl+C для завершения.' ); 
 });
 
-var fortunes = ["Победи свои страхи , "+
-                "или они победят тебя.",
-                "Рекам нужны истоки.",    
-                "Не бойся неведомого.",    
-                "Тебя ждет приятный сюрприз.",    
-                "Будь проще везде, где только можно.", 
-            ];
